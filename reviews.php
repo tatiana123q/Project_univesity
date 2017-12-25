6dc6bacd5c822c48e46ba4fc7e4fafa2aa7c9460
elleenchik <olenapankina@gmail.com>
2017-12-25 21:00:30 +0200

    minor changes

--- reviews.php
+++ reviews.php
@@ -128,27 +128,9 @@
 				<p>Какой-то текст, описывает какая классная у нас клиника, много качественных услуг она предлагает, и еще много много всего</p>
 			</div>
 		</div>
-		<form method="POST" action="index2.php" id="feedback-form">
-			<input required type="text" name="name" id="name" size="25" placeholder="Введите имя" />
-			<input required type="text" name="second_name" id="second_name" size="25" placeholder="Введите фамилию" /><br>
-			<input required type="number" name="phone" id="phone" size="25" placeholder="Введите телефон" />
-			<input required type="email" name="mail" id="mail" size="25" placeholder="Введите почту" /><br>
-
-			<select name="type_of_visitation">
-				<option selected="selected">Тип посещения</option>
-				<option >Снимок</option>
-				<option >Хирургия</option>
-				<option>Осмотр</option>
-			</select>
-			<select name="type_of_doctor" >
-				<option selected="selected">Выбор врача</option>
-				<option >Синенко А.Л.</option>
-				<option >Марченко С.Н</option>
-				<option>Кириенко Р.Т.</option>
-			</select><br>
-			<input type="date" name="date" id="date" min="2017-12-17"><br>
-			<button type="submit" form="feedback-form" value="Submit">Записаться</button>
-		</form>
+<?php 
+	require_once('/forma.php');
+?>
 	</section>
 	<button class="callback"><a id="popup__toggle"><img src="img/phone.png" width="90px" height="90px"></a></button>
 	<!--[if lt IE 9]><div class="popup__overlay popup__overlay_ie"></div><![endif]-->
