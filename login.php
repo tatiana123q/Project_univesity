14288a621d6126152c6afce5b2c8ddbc8f1f4cd9
Kotic Ivana <kotik.iv@ukr.net>
2017-12-26 03:23:02 +0200

    session changes

--- login.php
+++ login.php
@@ -1,19 +1,28 @@
-<a onclick="show('block')"><i class="fa fa-user" aria-hidden="true"></i></a>
-	<div onclick="show('none')" id="wrap"></div>
-		<div id="window">
-			<img class="close" onclick="show('none')" src="img/close.png" height="30" width="30">
-				<form method="POST" action="login2.php" id="backSignIn" name="backSignIn">
-					<div class="signin_main">
-						<input name="email" type="Email" id="EmailForm" maxlength="20" placeholder="Введите e-mail" required="required" />
-						<input name="phone" type="Tel" id="telForm" maxlength="20" placeholder="Введите пароль" required="required" />
-					</div>
-						<input id="telButton" type="submit" form="backSignIn" value="Войти"></input>
-				</form>	
+<?php session_start();
+ if($_SESSION['ID_User'])
+{
+?><a href="personal_area.php"><i class="fa fa-user" aria-hidden="true"></i></a><?
+} elseif (!$_SESSION['ID_User']) {
+	?><a onclick="show('block')"><i class="fa fa-user" aria-hidden="true"></i></a>
+		<div onclick="show('none')" id="wrap"></div>
+			<div id="window">
+				<img class="close" onclick="show('none')" src="img/close.png" height="30" width="30">
+					<form method="POST" action="login2.php" id="backSignIn" name="backSignIn">
+						<div class="signin_main">
+							<input name="email" type="Email" id="EmailForm" maxlength="20" placeholder="Введите e-mail" required="required" />
+							<input name="phone" type="Tel" id="telForm" maxlength="20" placeholder="Введите пароль" required="required" />
+						</div>
+							<input id="telButton" type="submit" form="backSignIn" value="Войти"></input>
+					</form>	
 
-		</div>
+			</div>
 		<script type="text/javascript">
 			function show(state){
 			document.getElementById('window').style.display = state;			
 			document.getElementById('wrap').style.display = state; 			
 			}
-		</script>
+</script><?
+}
+	else {?><a onclick="<?php session_destroy();?>"></a><? }
+?>
+
